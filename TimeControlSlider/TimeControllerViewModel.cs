047b9c8211016706bc53ec053dfc70f473d434ab
liavtr <52718075+liavtr@users.noreply.github.com>
2021-04-05 00:17:10 +0300

    wow im tired

---
+++ TimeControlSlider/TimeControllerViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeControlSlider
+{
+    public class TimeControllerViewModel : INotifyPropertyChanged
+    {
+        TimeControllerModel timeControllerModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if(this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public TimeControllerViewModel(TimeControllerModel timeControllerModel)
+        {
+            this.timeControllerModel = timeControllerModel;
+            timeControllerModel.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+        public void setMap(Dictionary<int, string> value)
+        {
+            timeControllerModel.setMap(value);
+        }
+        public int VM_Time
+        {
+            get
+            {
+                return timeControllerModel.Time;
+            }
+            set
+            {
+                timeControllerModel.Time = value;
+            }
+        }
+
+        public int VM_FlightLength
+        {
+            get
+            {
+                return timeControllerModel.FlightLength;
+            }
+        }
+
+        public int VM_Pace
+        {
+            get
+            {
+                return timeControllerModel.Pace;
+            }
+            set
+            {
+                timeControllerModel.Pace = value;
+            }
+        }
+
+        public void StartApp()
+        {
+            timeControllerModel.StartApp();
+        }
+        public void Run()
+        {
+            timeControllerModel.Run();
+        }
+
+        public void PauseFlight()
+        {
+            timeControllerModel.PauseFlight();
+        }
+
+        public void StartFlightOver()
+        {
+            timeControllerModel.StartFlightOver();
+        }
+
+        public void EditPace(int newPace)
+        {
+            timeControllerModel.EditPace(newPace);
+        }
+
+    }
+
+
+}
